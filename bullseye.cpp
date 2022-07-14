a71a029b8356b82a24cdd1dc240dfc85cf2d9a5e
EddieAlbusJiang <Eddie_shenan@126.com>
2022-07-14 19:06:36 +0800

    Version Dev1.0.2
    Basic configuration modification supported. Added the bullseye for testing purposes.

---
+++ bullseye.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Hit bullseye."<<endl;
+    cout<<"Current Directory: ";
+    system("cd");
+    cout<<"---------------------------"<<endl;
+    return 0;
+}
